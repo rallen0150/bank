c852a580d369724fccae81ef4b47f2ca8d544f4a
rallen0150 <rallen0150@gmail.com>
2016-10-28 18:28:03 -0400

    got a token to be added to a new created user

--- bank/models.py
+++ bank/models.py
@@ -2,10 +2,21 @@ from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+## Found this on a website to help generate a token to a new user
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 OPTION = [
-    ('W', 'withdraw'),
+    ('W', 'Withdraw'),
     ('D', 'Deposit'),
 ]
 
